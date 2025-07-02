968e2ceb1209ece55b91fbc2b3b253aa58d41554
teja <ramisettysuryateja9@gmail.com>
2025-07-02 18:26:02 +0530

    how are you feeling now

--- sample.py
+++ sample.py
@@ -1,3 +1,7 @@
-print ("hello world")
-
-echo (this is my_id)
+x = 7
+if x > 10:
+    print("x is greater than 10")
+elif x == 7:
+    print("x is equal to 7")
+else:
+    print("x is less than 10 and not equal to 7")
