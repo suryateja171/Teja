a2bde3b78d84d17b7c2eb1d3ef341eebc042c105
teja <ramisettysuryateja9@gmail.com>
2025-07-08 18:11:01 +0530

    need to switch it

---
+++ sample5.py
@@ -0,0 +1,31 @@
+# def preOrderTraversal(node):
+#     if node is None:
+#         return
+#     print(node.data, end=", ")
+#     preOrderTraversal(node.left)
+#     preOrderTraversal(node.right)
+
+
+
+
+
+
+# class Student:
+#     def __init__(self):
+#         self.__marks = 0
+
+#     def set_marks(self,marks):
+#         self.__marks = marks
+
+#     def get_marks(self):
+#         return self.__marks
+# s = Student()
+# s.set_marks(85)
+# print("Student Marks:", s.get_marks())
+# print(s.__marks)
+    
+
+
+
+
+    
