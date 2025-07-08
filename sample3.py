6d542f4e876a01d31bc8fc3e4a35c1b16fb2f344
teja <ramisettysuryateja9@gmail.com>
2025-07-08 17:06:57 +0530

    hello bachee

---
+++ sample3.py
@@ -0,0 +1,12 @@
+# class BankAccount:
+#     def __init__(self):
+#         self.__balance = 0
+#     def deposit(self,balance):
+#         self.__deposit += amount
+#     def get_balance(self):
+#         return self.__balance
+
+# account = BankAccount()
+# account.deposit(5000)
+# print("")          
+    
